667b71e84891ee87bee0e67be20831137587d115
Алина Манагадзе <agmanagadze_1@edu.hse.ru>
2019-05-27 12:48:31 +0500

    added

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -8,8 +8,53 @@ namespace task_3
 {
     class Program
     {
+        static void Color(string txt)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(txt);
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
+            double y = 0, x = 0;
+            bool ok = false;
+            do
+            {
+                Color("Введите х:");
+                try  // Ввод переменной x
+                {
+                    ok = true;
+                    x = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    ok = false;
+                    //ok = false;
+                    Console.WriteLine("Ошибка, повторите ввод");
+                }
+            } while (!ok);
+
+            Color("Введите у:");
+            do
+            {
+                try  // Ввод переменной y
+                {
+                    ok = true;
+                    y = double.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    ok = false;
+                    Console.WriteLine("Ошибка, повторите ввод");
+                }
+            } while (!ok);
+
+            if ((y <= -2 * x + 1) && (y <= 2 * x + 1) && (y >= -2 * x - 1) && (y >= 2 * x - 1))
+                Console.WriteLine($"Точка ({x};{y}) принадлежит данной области");
+            else Console.WriteLine($"Точка ({x};{y}) не принадлежит данной области");
+
+            Console.Read();
         }
     }
 }
